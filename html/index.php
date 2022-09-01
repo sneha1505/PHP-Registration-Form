77247291dfc715fa60feb14e85eeab52462189b9
sneha1505 <87608797+sneha1505@users.noreply.github.com>
2022-09-02 00:41:38 +0530

    Update index.php

--- html/index.php
+++ html/index.php
@@ -66,16 +66,14 @@ $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
     <!-- Jumbotron-->
     <div class="jumbotron">
         <h1>Hello, <?php echo $userRow['username']; ?></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at auctor est, in convallis eros. Nulla
-            facilisi. Donec ipsum nulla, hendrerit nec mauris vitae, lobortis egestas tortor. </p>
+        <p>Welcome to the Site, Great to see your here....<br>Please register yourself first!!!!<br>  </p>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
     </div>
 
     <div class="row">
         <div class="col-lg-12">
             <h2>Example body text</h2>
-            <p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et
-                magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
+            <p>Welcome to page.. Enter your details carefully!!!!</p>
             <p>
                 <small>This line of text is meant to be treated as fine print.</small>
             </p>
